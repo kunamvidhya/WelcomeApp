53cc612b4e072cd22795f48182058b6d7575cea1
kunamvidhya <153719078+kunamvidhya@users.noreply.github.com>
2024-02-15 12:17:59 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WelcomeApp;
+
+Console.WriteLine("Welcome to Authenticate App");
+Signin obj = new Signin();
+Console.WriteLine(Signin.Authenticate("admin", "admin12345"));
---
+++ Signin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class Signin
+    {
+        public static string Authenticate(String userName, string password)
+        {
+            string msg;
+
+            if ((string.IsNullOrEmpty(userName)) || string.IsNullOrEmpty(password))
+            {
+                msg = "User name and password required";
+            }
+            else
+            {
+                if ((userName == "admin") && (password == "admin12345"))
+                {
+                    msg = "Authentication Fail";
+                }
+                else
+                {
+                    msg = "Authentication Pass";
+                }
+            }
+            return msg;
+        }
+    }
+}
+
